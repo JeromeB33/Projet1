afbe90cb1e34c7c14d895b7c98deae58d577ae47
HEMMER-FX <hemmerfrancoisxavier@gmail.com>
2021-03-18 17:15:29 +0100

    mise en page histoire

---
+++ histoire.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
+    <link rel="stylesheet" media="screen" href="./style/style_histoire_mobile.css" type="text/css" />
+    <link rel="stylesheet" media="screen" href="./style/style_histoire_print.css" type="text/css" />
+</head>
+
+<body>
+    <div id="big_box_histoire"> <!-- grande div (barre nav + article-->
+            <nav>
+              <ul id="menu-deroulant"> <!-- barre de navigation mobile caché -->
+                  <li><a href="#" id="alt-titreHide">Histoire</a>
+                      <ul>
+                          <li><a href="index.php">Accueil</a></li>
+                          <li><a href="compétences.php">Compétences</a></li>
+                          <li><a href="articles.php">Articles</a></li>
+                          <li><a href="quizz.php">Quizz</a></li>
+                          <li><a href="contact.php">Contact</a></li>
+                      </ul>
+                  </li>
+              </ul>
+            </nav>
+
+        <div id="section_flex">
+                 <div id="section_presentation">
+                      <h3> Histoire</h3>
+                      <p> De Eric Hochet</p>
+                </div>
+
+                <div class="section_presentation">
+                    <h3> Mon enfance </h3>
+                    <p> Tout à commencé en 1992 avce une place</p>
+
+                </div>
+
+                <div class="section_presentation">
+                    <h3> Mon adolescence </h3>
+                    <p> Au collège j'ai eu mes premiers poils Au collège j'ai eu mes premiers poils pubiens que je jetais dlorem sur les genspubiens que je jetais dlorem sur les gens</p>
+                </div>
+
+                <div class="section_presentation">
+                    <h3> Mon parcours professionel </h3>
+                    <p> Quand j'ai commencé dans ce fameux restaurant </p>
+                </div>
+        </div>
+
+    </div>
+
+</body>
+
+<footer> <!-- Le footer commun à toutes les pages -->
+    <p>
+        Eric Hochet <br />
+        Telephone : 36.15 <br />
+        E-mail :
+        <a href="mailto:eric.hochet@gmail.com">armand.hinz@gmail.com</a>
+        <br><br>
+        <span id="disclaimer">Ce site est en construction dans le cadre d'un projet pour la Wild Code School</span>
+    </p>
+
+</footer>
+</html>
