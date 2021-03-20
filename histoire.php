8f790c0ed69f4c1b432c832d6504ac8e26907f8b
HEMMER-FX <hemmerfrancoisxavier@gmail.com>
2021-03-20 09:28:35 +0100

    maj fichier

--- histoire.php
+++ histoire.php
@@ -9,63 +9,75 @@
     <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
-    <link rel="stylesheet" media="screen" href="./style/style_histoire_mobile.css" type="text/css" />
-    <link rel="stylesheet" media="screen" href="./style/style_histoire_print.css" type="text/css" />
+    <link rel="stylesheet" media="screen" href="style/style_histoire_mobile.css" type="text/css" />
+    <link rel="stylesheet" media="screen" href="style/style_histoire_print.css" type="text/css" />
 </head>
 
 <body>
-    <div id="big_box_histoire"> <!-- grande div (barre nav + article-->
-            <nav>
-              <ul id="menu-deroulant"> <!-- barre de navigation mobile caché -->
-                  <li><a href="#" id="alt-titreHide">Histoire</a>
-                      <ul>
-                          <li><a href="index.php">Accueil</a></li>
-                          <li><a href="compétences.php">Compétences</a></li>
-                          <li><a href="articles.php">Articles</a></li>
-                          <li><a href="quizz.php">Quizz</a></li>
-                          <li><a href="contact.php">Contact</a></li>
-                      </ul>
-                  </li>
-              </ul>
-            </nav>
+<!-- grand div pour le contenu -->
+<div id="grandContenaire">
+    <nav>   <!-- barre de navigation idem toutes pages -->
+        <ul id="menu-deroulant">
+            <li><a href="#" id="alt-titreHide">Histoire</a> <!-- id pour cacher le tire -->
+                <ul>
+                    <li><a href="index.php">Accueil</a></li>
+                    <li><a href="compétences.php">Compétences</a></li>
+                    <li><a href="articles.php">Article</a></li>
+                    <li><a href="quizz.php">Quizz</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
 
-        <div id="section_flex">
-                 <div id="section_presentation">
-                      <h3> L'histoire</h3>
-                      <p> De Eric Hochet</p>
-                </div>
+    <div id="section_histoire"> <!-- div pour placer les sections -->
+        <header> <!-- pour placer le tire de la page -->
+            <h1 id="titre">Histoire</h1>
+        </header>
 
-                <div class="section_presentation">
-                    <h3> Mon enfance </h3>
-                    <p> Tout à commencé en 1992 avce une placeTout à commencé en 1992 avce une placeTout à commencé en 1992 avce une placeTout à commencé en 1992 avce une placeTout à commencé en 1992 avce une placeTout à commencé en 1992 avce une placeTout à commencé en 1992 avce une placeTout à commencé en 1992 avce une placeTout à commencé en 1992 avce une place</p>
+        <main> <!-- Le contenu principal de notre page -->
 
-                </div>
+            <div class="contenu_main">
+                <h2>Le fossé</h2>
+                <p> Eric Hochet a physiquement interagit avec un autre humain pour la première fois depuis cinq mois.<br>
+                    Interaction qu’il a complètement gâchée en centrant la conversation entièrement sur lui-même.</p>
+            </div>
 
-                <div class="section_presentation">
-                    <h3> Mon adolescence </h3>
-                    <p> Au collège j'ai eu mes premiers poils Au collège j'ai eu mes premiers poils pubiens que je jetais dlorem sur les genspubiens que je jetais dlorem sur les gensTout à commencé en 1992 avce une placeTout à commencé en 1992 avce une placeTout à commencé en 1992 avce une placeTout à commencé en 1992 avce une placeTout à commencé en 1992 avce une placeTout à commencé en 1992 avce une place</p>
-                </div>
+            <div class="contenu_main">
+                <h2>Le fossé</h2>
+                <p> Eric Hochet a physiquement interagit avec un autre humain pour la première fois depuis cinq mois.<br>
+                    Interaction qu’il a complètement gâchée en centrant la conversation entièrement sur lui-même.</p>
+            </div>
 
-                <div class="section_presentation">
-                    <h3> Mon parcours professionel </h3>
-                    <p> Quand j'ai commencé dans ce fameux restaurantTout à commencé en 1992 avce une placeTout à commencé en 1992 avce une placeTout à commencé en 1992 avce une placeTout à commencé en 1992 avce une placeTout à commencé en 1992 avce une placeTout à commencé en 1992 avce une placeTout à commencé en 1992 avce une placeTout à commencé en 1992 avce une placeTout à commencé en 1992 avce une placeTout à commencé en 1992 avce une placeTout à commencé en 1992 avce une placeTout à commencé en 1992 avce une place </p>
-                </div>
+            <div class="contenu_main">
+                <h2>Le fossé</h2>
+                <p> Eric Hochet a physiquement interagit avec un autre humain pour la première fois depuis cinq mois.<br>
+                    Interaction qu’il a complètement gâchée en centrant la conversation entièrement sur lui-même.</p>
+            </div>
 
-        </div>
+            <div class="contenu_main">
+                <h2>Le fossé</h2>
+                <p> Eric Hochet a physiquement interagit avec un autre humain pour la première fois depuis cinq mois.<br>
+                    Interaction qu’il a complètement gâchée en centrant la conversation entièrement sur lui-même.</p>
+            </div>
 
+        </main>
     </div>
 
-</body>
+</div>
+
 
 <footer> <!-- Le footer commun à toutes les pages -->
     <p>
         Eric Hochet <br />
         Telephone : 36.15 <br />
         E-mail :
-        <a href="mailto:eric.hochet@gmail.com">armand.hinz@gmail.com</a>
+        <a href="mailto:eric.hochet@gmail.com">eric.hochet@gmail.com</a>
         <br><br>
-        <span id="disclaimer">Ce site est en construction dans le cadre d'un projet pour la Wild Code School</span>
+        <span id="disclaimer">Ce site est en contruction dans le cadre d'un projet pour la Wild Code School</span>
     </p>
 
 </footer>
+
+</body>
 </html>
