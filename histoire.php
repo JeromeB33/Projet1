3a5316cde9786dfae3ebb0c2774060cf3f646469
HEMMER-FX <hemmerfrancoisxavier@gmail.com>
2021-03-24 11:10:17 +0100

    histoire maj

--- histoire.php
+++ histoire.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Accueil</title>
+    <title>Histoire de Eric HOCHET</title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.gstatic.com">
@@ -39,30 +39,35 @@
 
             <div class="contenu_main">
                 <h2>Naissance</h2>
-                <p> lorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem
-                    lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem
-                    loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem lorem</p>
+                <p> C'est le 28 octobre 1984 que j'ai vu le jour. En effet mes parents Nadine Amo ainsi que mon père Bale Hochet son fier de présenter à toute la famille ma venu, cela faisait trop longtemps que mes parents m'attendaient. Mes parents sont passés
+                par des épreuves de la vie très compliqué, ma maman était enceinte un peu avant de m'avoir mais malencontreusement cela s'est mal passé car pour son anniversaire mon père lui a offert un saut en élastique et au moment de remonté elle à vu quelque chose
+                tomber, c'est à ce moment la que'elle a su qu'elle était enceinte. Depuis pour sa nouvelle grossesse ma maman est restée à la maison .</p>
             </div>
 
             <div class="contenu_main">
                 <h2>Collège</h2>
-                <p> lorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem
-                    lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem
-                    loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem lorem</p>
+                <p> A l'âge de 12 ans je suis rentré au collège , j'ai toujours était un enfant malicieux et très intélligent , mon père me disait toujours "Tu compris vite filston , mais faut t'expliquer longtemps". Je suis allé au collège de Francacatastrophe , c'était
+                un petit collège de 300 collégiens et autour il n'y avait que des champs, l'on pouvait percevoir les paysans du coins s'occuper de leurs vaches!
+                J'ai vraiment aimé mes 5 années de collège car j'ai toujours était bien entouré de copains, que ce soit en classe de 6e bleu , mes deux 5e blanc , 4e blanc et en 3e rouge.
+                D'ailleurs c'est fou le changement de la petite école au collège, on apprends plein de nouvelle matière, ma préférée était l'allemand mais on m'a interdit d'allemand, quand je parlais je postillonné sur mes camarades.</p>
             </div>
 
             <div class="contenu_main">
                 <h2>Lycée</h2>
-                <p> lorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem
-                    lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem
-                    loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem lorem</p>
+                <p> Alors la par ou commencer ? Mon premier jours au lycée fut magnifique, après avoir passé quelques mois loin de ma chérie "Élie Coptère" ( mais elle ne savait pas que je l'aimais au collège) je l'ai enfin revue, elle a fichtrement bien changée
+                je savais pas que des nénés pouvaient pousser si vite (c'est peut être comme les légumes, mon père mets des pesticides il dis que sa repousse les bactéries et que ça donne un meilleur goût.)
+                C'est au lycée que j'ai rencontré mon meilleur ami, "Clint Horris" il a grandi aux USA mais ses parents sont venus habiter en France, des que je lui ai parlé on a tout de suite accroché ensemble, il est juste plus agé que moi car il a 31 ans, tout le monde
+                se moqué de lui à cause de son âge mais les gens ne savent pas pourquoi il est au collège, c'est parce qu'il veut connaitre tout ses cours par coeur avant de partir du lycée, moi je trouve que c'est un malin.</p>
             </div>
 
             <div class="contenu_main">
                 <h2>Vie professionnelle</h2>
-                <p> lorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem
-                    lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem
-                    loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem loremlorem lorem lorem</p>
+                <p> Je suis sorti plus tôt que mon ami Clint Horris car je n'ai pas la patience d'apprendre tout mes cours, j'aime charbonner et bouger mes doigts. A ma sortie de lycée j'avais 22 ans (et encore toutes mes dents ) je dis ça car Clint Horris lui il les perds
+                il me dit que c'est à cause du stress des cours mais j'ai un doute je pense plutôt  que c'est parce qu'il se brosse les dents avec des cigarettes qu'il a fumé. En tout cas voilà des ma sortie de lycée je suis allé travailler chez Roland Culé, c'est un agriculteur
+                qui vit a côté de la maison, il fait pousser des plantes médicinales qui se vendent très chères, il a beaucoup de client et moi j'ai beaucoup à apprendre de lui. Après quelques années passées à ses côtés je me dois de changer de métier car il y a des jours
+                je crois que je ne supporte plus ce que Roland Culé fait, il brûle les mauvaises herbes mais moi après j'ai souvent envie de dormir. D'après mes parents c'est parce que le métier ne m'intéresse plus assez. C'est dommage je l'aimais bien mon patron et ce qu'il
+                fallait faire. Je pense que je vais me diriger vers une nouvelle voix, j'aime mon village et je pense que je vais me lancer dans la propreté du village, je commencerai par faire les trotoires, comme ca je dirais à toutes les personnes que je croiserai d'arrêter
+                de jeter leurs chewing gum avec le goût bizare, j'en ai une fois ramassé un pour le gouter c'était pas bon , il avait un goût très âpre et la jus très salé.</p>
             </div>
 
         </main>
