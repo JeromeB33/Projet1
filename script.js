e9decf1373558758edc40594903ea8a0f9a74033
ArmandHinz <Armand.hinz@gmail.com>
2021-03-24 14:39:30 +0100

    ajout des sons

--- script.js
+++ script.js
@@ -1,50 +1,24 @@
-/*function init() {
-    var article1 = new Audio("./Sons/Articles-intro.mp3");
-    var article2 = new Audio('./Sons/Intro-eric.mp3');
-  
-    article1.loop = false;
-    article2.loop = false;
-  
-    function once1() {  
-    article1.load();
-    article1.play(); 
-    }
-    function once2() {
-    article2.play(); 
-      }
-    var btn1 = document.getElementById("test1");
-    btn1.addEventListener('click', once1);
-  
-    var btn2 = document.getElementById("test2");
-    btn2.addEventListener('click', once2);
-  }
-*/
-/*
-function playAudio(){
-    let son1= new audio("./Sons/Articles-intro.mp3");
 
-    let buttonSon= document.getElementById("test1");
-    buttonSon.addEventListener('click', son1.play());
-}*/
-
-/*
-playSound = function () {
-    var audio = new Audio('./Sons/Articles-intro.mp3');
+function audioPlay1() {
+    var audio = new Audio('../Sons/article1.mp3');
     audio.loop = false;
     audio.play(); 
-}*/
-/*
-function JouerSon() {
-    var sound = document.getElementById("son1");
-    sound.play();
-}*/
-$(document).ready(function(){
-  $('#TonImage').click(function(){
-      var song = $('audio');
-      if(song.paused){ 
-          song.play()
-      } else {
-          song.pause()
-      }
-  })
-})
+}
+function audioPlay2() {
+  var audio = new Audio('../Sons/article2.mp3');
+  audio.loop = false;
+  audio.play(); 
+}
+function audioPlay3() {
+  var audio = new Audio('../Sons/article3.mp3');
+  audio.loop = false;
+  audio.play(); 
+}
+function audioPlay4() {
+  var audio = new Audio('../Sons/article4.mp3');
+  audio.loop = false;
+  audio.play(); 
+}
+
+
+
